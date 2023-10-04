1418339458df0a38c6807a7441938edb4798e12c
AARMCAS <aarmcas@alumno.upv.es@ldsic-vdi14.upvnet.upv.es>
2023-10-04 21:00:39 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -9,20 +9,45 @@ class ListArray : public List<T> {
 	int max;
 	int n;
 	static const int MINSIZE;
+        void resize(int new_size){
+	     if (new_size < MINSIZE)
+                  new_size = MINSIZE;
+	     T* new_arr = new T[new_size];
+	     for (int i = 0; i < n; i++) 
+                  new_arr[i] = arr[i];
+	     delete[] arr; 
+             arr = new_arr; 
+             max = new_size; 
+	}
 
     public:
         ListArray(){
 	    max = 0;
+	    arr = new T[max];
 	    n = 0;
+	    MINSIZE = 2;
 	}
 
         ~ListArray(){
+	    delete[] arr;
 	}
 
 	T operator[](int pos){
 	    if (pos < 0 || pos >= max)
-            throw std::out_of_range("Posición inválida");
-
-
+                throw std::out_of_range("Índice fuera de rango");
+	    return arr[pos];
 	}
+
+	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+	    out << "[";
+            for (int i = 0; i < list.n; ++i) {
+                 out << list.arr[i];
+                 if (i < list.n - 1) 
+                      out << " ";
+            }
+            out << "]";
+            return out;
+        }
+
+
 };
