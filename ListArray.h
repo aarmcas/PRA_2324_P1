1e113b0fd38d43570b3464ac31377f35e42b5b71
AARMCAS <aarmcas@alumno.upv.es@ldsic-vdi03.upvnet.upv.es>
2023-10-05 12:30:59 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -25,74 +25,15 @@ class ListArray : public List<T> {
 	    max = 0;
 	    arr = new T[max];
 	    n = 0;
-	    MINSIZE = 2;
+	    MINSIZE;
 	}
 
         ~ListArray(){
 	    delete[] arr;
 	}
 
-	void insert(int pos, T e) override {
-	    if (pos < 0 || pos > max - 1) 
-                throw std::out_of_range("Índice fuera de rango");
-            if (n == max) 
-                resize(max + 1);
-	    for (int i = n; i > pos; i--) 
-                arr[i] = arr[i - 1];
-	    arr[pos] = e;
-            n++;
-	}
-
-	void append(T e) override {
-            if (n == max)
-                resize(max + 1);
-            arr[n] = e;
-            n++;
-	}
-
-	void prepend(T e) override {
-	    if (n == max)
-                resize(max + 1);
-            for (int i = n; i > 0; i--)
-                arr[i] = arr[i - 1];
-	    arr[0] = e;
-	    n++;
-	}
-
-        T remove(int pos) override {
-            if (pos < 0 || pos > max - 1)
-                throw std::out_of_range("Índice fuera de rango");
-            T removedElement = arr[pos];
-	    for (int i = pos; i < n - 1; i++) 
-                arr[i] = arr[i + 1];
-	    n--;
-	    return removedElement;
-	}
-
-	T get(int pos) override {
-	    if (pos < 0 || pos > max - 1)
-                throw std::out_of_range("Índice fuera de rango");
-            return arr[pos];
-	}
-
-	int search(T e) override {
-	    for (int i = 0; i < n; i++) {
-                if (arr[i] == e) 
-                    return i; 
-	    }
-	    return -1;
-	}
-
-	bool empty() override {
-	    return n == 0;
-	}
-
-	int size() override {
-	    return n;
-	}
-
 	T operator[](int pos){
-	    if (pos < 0 || pos > max - 1)
+	    if (pos < 0 || pos >= max)
                 throw std::out_of_range("Índice fuera de rango");
 	    return arr[pos];
 	}
